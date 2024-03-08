26b4e297d602e318c58f4269b45843d55abc8eb0
TheLukCraft <luk1997r@o2.pl>
2024-03-08 08:02:04 +0100

    connection string

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,9 +1,14 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
     public class Category
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public int DisplayOrder { get; set; }
     }
 }
