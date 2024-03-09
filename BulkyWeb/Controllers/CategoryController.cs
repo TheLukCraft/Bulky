094727932a4ab4c3bb7fb8611f607fd66bd579f6
TheLukCraft <luk1997r@o2.pl>
2024-03-09 09:04:23 +0100

    Create Category UI

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -17,5 +17,10 @@ namespace BulkyWeb.Controllers
             var objCategoryList = db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
