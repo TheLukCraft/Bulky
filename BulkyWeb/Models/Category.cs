cb33767472d031194174195849d1f88535d40c73
TheLukCraft <luk1997r@o2.pl>
2024-03-09 09:10:05 +0100

    Input Tag Helpers

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -7,8 +8,10 @@ namespace BulkyWeb.Models
         public int Id { get; set; }
 
         [Required]
+        [DisplayName("Category order")]
         public string Name { get; set; }
 
+        [DisplayName("Display order")]
         public int DisplayOrder { get; set; }
     }
 }
